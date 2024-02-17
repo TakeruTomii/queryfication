e41bc92afd0ed58ca4a443c8ba028f59f33cb4d2
TakeruTomii <dev.haunted.house@gmail.com>
2024-02-18 10:34:22 +1300

    add display on List page

--- src/Queries/List/List.js
+++ src/Queries/List/List.js
@@ -1,14 +1,28 @@
+import SampleData from "../../Asset/SampleData.json";
+import "./List.css";
+
 export const List = function List() {
-    return (
-      <div className="List">
-        <div id="seach-box">
-            TODO: add search box
-        </div>
-        <hr></hr>
-        <div id="query-list">
-            TODO: add list of queries
-        </div>
+  let list = [];
+
+  for (const [i, data] of SampleData.entries()) {
+    list.push(
+      <div class="list-item">
+        <p class="title">{data["title"]}</p>
+        <div>{data["query"]}</div>
+        <p>
+          Star:{data["averageReview"]} ({data["reviewers"]})
+        </p>
       </div>
     );
   }
-  
+
+  return (
+    <div className="List">
+      <div id="search-box">
+        <input type="text" />
+      </div>
+      <hr></hr>
+      <div id="query-list">{list}</div>
+    </div>
+  );
+};
