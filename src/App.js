93ef1dae2b1cd308ae67f0e39848b2827a319f90
TakeruTomii <dev.haunted.house@gmail.com>
2024-02-18 00:23:29 +1300

    add list page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { List } from "./List/List"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Queryfication
+        <BrowserRouter>
+        <Link to="/list">List</Link>
+        <br />
+
+        <Routes>
+          <Route path="/list" element={<List />} />
+        </Routes>
+    </BrowserRouter>
       </header>
+      <div>
+        <h1>Welcome to Queryfication!</h1>
+        <p>
+          This site is created for prompt engineers to share your own useful query for chat-gpt or similar platform.
+        </p>
+        <p>
+          You suppose to:
+          <ul>
+            <li>Share your query with examples.</li>
+            <li>Evaluate the query created by someone else.</li>
+            <li>Search the queries posted here to use your convenience.</li>
+          </ul>
+          Get some sophisticated queries and aim to create epoc-making prompts!
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/List/List.js
@@ -0,0 +1,14 @@
+export const List = function List() {
+    return (
+      <div className="List">
+        <div id="seach-box">
+            TODO: add search box
+        </div>
+        <hr></hr>
+        <div id="query-list">
+            TODO: add list of queries
+        </div>
+      </div>
+    );
+  }
+  
