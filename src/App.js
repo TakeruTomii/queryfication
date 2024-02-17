1823ef9acaf7d2a1d5a6cd9f07eafb4de7fe3600
TakeruTomii <dev.haunted.house@gmail.com>
2024-02-18 08:46:19 +1300

    add transitions

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import { List } from "./List/List"
+import { List } from "./List/List";
+import {Home} from "./Home/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Queryfication
-        <BrowserRouter>
+
+      </header>
+      <BrowserRouter>
+        <Link to="/">Home</Link>
         <Link to="/list">List</Link>
         <br />
-
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/list" element={<List />} />
         </Routes>
-    </BrowserRouter>
-      </header>
-      <div>
-        <h1>Welcome to Queryfication!</h1>
-        <p>
-          This site is created for prompt engineers to share your own useful query for chat-gpt or similar platform.
-        </p>
-        <p>
-          You suppose to:
-          <ul>
-            <li>Share your query with examples.</li>
-            <li>Evaluate the query created by someone else.</li>
-            <li>Search the queries posted here to use your convenience.</li>
-          </ul>
-          Get some sophisticated queries and aim to create epoc-making prompts!
-        </p>
-      </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,22 @@
+export const Home = function Home() {
+  return (
+    <div className="Home">
+      <div>
+        <h1>Welcome to Queryfication!</h1>
+        <p>
+          This site is created for prompt engineers to share your own useful
+          query for chat-gpt or similar platform.
+        </p>
+        <p>
+          You suppose to:
+          <ul>
+            <li>Share your query with examples.</li>
+            <li>Evaluate the query created by someone else.</li>
+            <li>Search the queries posted here to use your convenience.</li>
+          </ul>
+          Get some sophisticated queries and aim to create epoc-making prompts!
+        </p>
+      </div>
+    </div>
+  );
+}
